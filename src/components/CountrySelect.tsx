417c2d20c24bf4a6e26870e4b708821b155507be
Ed Shortt <dev@edshortt.co.uk>
2021-12-07 12:02:58 +0000

    Added country select

---
+++ src/components/CountrySelect.tsx
@@ -0,0 +1,79 @@
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import React, { ChangeEvent, SyntheticEvent } from 'react';
+import apiClient from './apiClient';
+
+export type CountryType = {
+    name: string;
+    code: string;
+};
+
+export type CountrySelectProps = {
+    className?: string;
+    name?: string
+    onChange?: (event: ChangeEvent<{}>, value: CountryType | null) => void;
+};
+
+export default function CountrySelect(props: CountrySelectProps) {
+    const [open, setOpen] = React.useState(false);
+    const [options, setOptions] = React.useState<CountryType[]>([]);
+    const loading = open && options.length === 0;
+
+    React.useEffect(() => {
+        if (!loading) {
+            return undefined;
+        }
+        apiClient
+            .get('/countries', {})
+            .then((res) => {
+                const countries = res.data;
+                setOptions(countries);
+            })
+    }, [loading]);
+
+    React.useEffect(() => {
+        if (!open) {
+            setOptions([]);
+        }
+    }, [open]);
+
+    return (
+        <Autocomplete
+            className="countrySelect"
+            open={open}
+            onOpen={() => {
+                setOpen(true);
+            }}
+            onClose={() => {
+                setOpen(false);
+            }}
+            getOptionSelected={(option, value) => option.name === value.name}
+            getOptionLabel={(option) => option.name}
+            onChange={props.onChange}
+            options={options}
+            loading={loading}
+            renderInput={(params) => (
+                <TextField
+                    {...params}
+                    name={props.name}
+                    label="Country"
+                    variant="filled"
+
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    InputProps={{
+                        ...params.InputProps,
+                        endAdornment: (
+                            <React.Fragment>
+                                {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                                {params.InputProps.endAdornment}
+                            </React.Fragment>
+                        ),
+                    }}
+                />
+            )}
+        />
+    );
+}
