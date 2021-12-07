f50ca85a0f1ae8619bacd8866ca65d7b66dce0c9
Ed Shortt <dev@edshortt.co.uk>
2021-12-07 12:00:53 +0000

    Updated theme

--- src/theme.ts
+++ src/theme.ts
@@ -6,7 +6,7 @@ const theme = createTheme({
     type: 'dark',
     primary: {
       // light: will be calculated from palette.primary.main,
-      main: '#0091ff',
+      main: '#00bcd4',
       // dark: will be calculated from palette.primary.main,
       // contrastText: will be calculated to contrast with palette.primary.main
     },
