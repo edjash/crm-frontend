016b76ff5e5eea1e34a29612448419f9b890f3e2
Ed Shortt <dev@edshortt.co.uk>
2021-12-09 16:39:22 +0000

    Deployment changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  if (mode === 'staging') {
+    return {
+      base: '/',
+      plugins: [react()],
+      build: {
+        outDir: '../server/public/frontend',
+        assetsDir: '',
+        emptyOutDir: true,
+      },
+    };
+  } else {
+    return {
+      base: '/',
+      plugins: [react()],
+    };
+  }
 });
