b850078189e9ab67a47e743e835cd7524ec4f26e
Ed Shortt <dev@edshortt.co.uk>
2021-12-09 12:07:34 +0000

    Housekeeping

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,10 @@
 /// <reference types="vite/client" />
-interface ImportMetaEnv extends Readonly<Record<string, string>> {
-    readonly VITE_API_URL: string
-    // more env variables...
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv
-  }
+interface ImportMetaEnv
+  extends Readonly<Record<string, string | boolean | undefined>> {
+  readonly VITE_API_URL: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
