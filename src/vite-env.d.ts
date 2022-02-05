5b96f30e9516fa8369ea5c598b176c4b38f27349
Ed Shortt <edjash@gmail.com>
2022-02-05 20:49:26 +0000

    Changed API_URL

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv
   extends Readonly<Record<string, string | boolean | undefined>> {
-  readonly VITE_API_URL: string;
+  readonly VITE_SERVER_URL: string;
   readonly VITE_APP_TITLE: string;
   // more env variables...
 }
