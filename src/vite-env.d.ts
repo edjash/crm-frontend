b186d29a87af4287b30fe9f27d6727bdafbb1598
Ed Shortt <edjash@gmail.com>
2022-02-10 06:20:43 +0000

    Added MODE variable

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,7 @@ interface ImportMetaEnv
   extends Readonly<Record<string, string | boolean | undefined>> {
   readonly VITE_SERVER_URL: string;
   readonly VITE_APP_TITLE: string;
+  readonly VITE_MODE: string;
   // more env variables...
 }
 
