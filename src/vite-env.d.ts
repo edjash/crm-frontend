00c111401ccbebf1933554f1575fadb64fe9f8fa
Ed Shortt <edjash@gmail.com>
2022-02-27 16:07:24 +0000

    Added APP_URL env variable

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,7 @@
 interface ImportMetaEnv
   extends Readonly<Record<string, string | boolean | undefined>> {
   readonly VITE_SERVER_URL: string;
+  readonly VITE_APP_URL: string;
   readonly VITE_APP_TITLE: string;
   readonly VITE_MODE: string;
   // more env variables...
