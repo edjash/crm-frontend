0dc6d9963392fcfe42a6cf3c058c6eb0f871993a
Ed Shortt <edjash@gmail.com>
2022-03-03 22:15:10 +0000

    fixed rhf causing input autofocus

--- src/components/Form/TextFieldEx.tsx
+++ src/components/Form/TextFieldEx.tsx
@@ -1,5 +1,5 @@
 import TextField, { TextFieldProps } from "@mui/material/TextField";
-import { useFormContext } from "react-hook-form";
+import { Controller, useFormContext } from "react-hook-form";
 
 type TextFieldExProps = TextFieldProps & {
     name: string;
@@ -7,24 +7,34 @@ type TextFieldExProps = TextFieldProps & {
 }
 
 export default function TextFieldEx(props: TextFieldExProps) {
-    const { register, formState } = useFormContext();
+    const { register, formState, control } = useFormContext();
 
     let errorMsg = formState.errors?.[props.name]?.message;
 
+    //console.log(props.name, props);
     return (
-        <TextField
-            {...props}
-            sx={{
-                display: props.hidden ? 'none' : 'block',
-                ...props.sx
-            }}
-            {...register(props.name)}
-            error={!!props?.error || !!errorMsg}
-            helperText={errorMsg || props?.helperText}
-            InputProps={{
-                required: false,
-                ...props.InputProps
-            }}
+        <Controller
+            render={({ field }) => (
+                <TextField
+                    {...props}
+                    {...field}
+                    sx={{
+                        display: props.hidden ? 'none' : 'block',
+                        ...props.sx
+                    }}
+                    //{...register(props.name)}
+                    value={field.value ?? ''}
+                    error={!!props?.error || !!errorMsg}
+                    helperText={errorMsg || props?.helperText}
+                    InputProps={{
+                        required: false,
+                        ...props.InputProps
+                    }}
+                />
+            )}
+            control={control}
+            name={props.name}
+            defaultValue={props?.defaultValue ?? ''}
         />
     );
 }
