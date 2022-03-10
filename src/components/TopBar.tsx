267740a8119783aad04f1563978ae12a09e86b0a
Ed Shortt <edjash@gmail.com>
2022-03-10 01:25:43 +0000

    Added logout button

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,20 +1,10 @@
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import PersonIcon from '@mui/icons-material/Person';
-import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
-import { SxProps, Theme, useMediaQuery } from '@mui/material';
+import LogoutIcon from '@mui/icons-material/Logout';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Button, SxProps, Theme, useMediaQuery } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Popover from '@mui/material/Popover';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import {
-    bindPopover, bindTrigger, usePopupState
-} from 'material-ui-popup-state/hooks';
-import MenuIcon from '@mui/icons-material/Menu';
 
 interface TopBarProps {
     sx?: SxProps,
@@ -25,13 +15,7 @@ export default function TopBar(props: TopBarProps) {
 
     const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
 
-    const accountMenuState = usePopupState({
-        variant: 'popover',
-        popupId: 'accountMenu',
-    });
-
     const handleLogout = () => {
-        accountMenuState.close();
         PubSub.publish('AUTH.LOGOUT');
     };
 
@@ -53,25 +37,14 @@ export default function TopBar(props: TopBarProps) {
                 </Typography>
                 <span className="spacer" />
                 <div>
-                    <IconButton {...bindTrigger(accountMenuState)}>
-                        <AccountCircle />
-                    </IconButton>
-                    <Popover
-                        {...bindPopover(accountMenuState)}
-                        anchorOrigin={{ vertical: 'center', horizontal: 'right', }}
-                        transformOrigin={{ vertical: 'top', horizontal: 'right', }}
-                    >
-                        <List dense={true} className="menuList">
-                            <ListItem button onClick={handleLogout}>
-                                <ListItemIcon><PowerSettingsNewIcon fontSize="small" /></ListItemIcon>
-                                <ListItemText primary="Logout" />
-                            </ListItem>
-                            <ListItem button onClick={accountMenuState.close} disabled={true}>
-                                <ListItemIcon><PersonIcon fontSize="small" /></ListItemIcon>
-                                <ListItemText primary="My Account" />
-                            </ListItem>
-                        </List>
-                    </Popover>
+                    {!isMobile &&
+                        <Button
+                            variant="outlined"
+                            onClick={handleLogout}
+                            startIcon={<LogoutIcon />}>
+                            Logout
+                        </Button>
+                    }
                 </div>
             </Toolbar>
         </AppBar>
