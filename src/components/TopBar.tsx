cbfbaad58e2303de40479bb6843ab679944d15c6
Ed Shortt <edjash@gmail.com>
2022-03-10 00:04:03 +0000

    Fixed formatting

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -12,68 +12,68 @@ import Popover from '@mui/material/Popover';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import {
-  bindPopover, bindTrigger, usePopupState
+    bindPopover, bindTrigger, usePopupState
 } from 'material-ui-popup-state/hooks';
 import MenuIcon from '@mui/icons-material/Menu';
 
 interface TopBarProps {
-  sx?: SxProps,
-  onNavToggleClick?: () => void,
+    sx?: SxProps,
+    onNavToggleClick?: () => void,
 }
 
 export default function TopBar(props: TopBarProps) {
 
-  const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
+    const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
 
-  const accountMenuState = usePopupState({
-    variant: 'popover',
-    popupId: 'accountMenu',
-  });
+    const accountMenuState = usePopupState({
+        variant: 'popover',
+        popupId: 'accountMenu',
+    });
 
-  const handleLogout = () => {
-    accountMenuState.close();
-    PubSub.publish('AUTH.LOGOUT');
-  };
+    const handleLogout = () => {
+        accountMenuState.close();
+        PubSub.publish('AUTH.LOGOUT');
+    };
 
-  return (
-    <AppBar position="fixed" elevation={1} sx={{ ...props.sx }}>
-      <Toolbar disableGutters sx={{ ml: 2, mr: 2 }}>
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          onClick={props.onNavToggleClick}
-          edge="start"
-          sx={{ mr: 2 }}
-          size="small"
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h5" sx={{ flexGrow: 1 }}>
-          Demo Company
-        </Typography>
-        <span className="spacer" />
-        <div>
-          <IconButton {...bindTrigger(accountMenuState)}>
-            <AccountCircle />
-          </IconButton>
-          <Popover
-            {...bindPopover(accountMenuState)}
-            anchorOrigin={{ vertical: 'center', horizontal: 'right', }}
-            transformOrigin={{ vertical: 'top', horizontal: 'right', }}
-          >
-            <List dense={true} className="menuList">
-              <ListItem button onClick={handleLogout}>
-                <ListItemIcon><PowerSettingsNewIcon fontSize="small" /></ListItemIcon>
-                <ListItemText primary="Logout" />
-              </ListItem>
-              <ListItem button onClick={accountMenuState.close} disabled={true}>
-                <ListItemIcon><PersonIcon fontSize="small" /></ListItemIcon>
-                <ListItemText primary="My Account" />
-              </ListItem>
-            </List>
-          </Popover>
-        </div>
-      </Toolbar>
-    </AppBar>
-  );
+    return (
+        <AppBar position="fixed" elevation={1} sx={{ ...props.sx }}>
+            <Toolbar disableGutters sx={{ ml: 2, mr: 2 }}>
+                <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={props.onNavToggleClick}
+                    edge="start"
+                    sx={{ mr: 2 }}
+                    size="small"
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h5" sx={{ flexGrow: 1 }}>
+                    Demo Company
+                </Typography>
+                <span className="spacer" />
+                <div>
+                    <IconButton {...bindTrigger(accountMenuState)}>
+                        <AccountCircle />
+                    </IconButton>
+                    <Popover
+                        {...bindPopover(accountMenuState)}
+                        anchorOrigin={{ vertical: 'center', horizontal: 'right', }}
+                        transformOrigin={{ vertical: 'top', horizontal: 'right', }}
+                    >
+                        <List dense={true} className="menuList">
+                            <ListItem button onClick={handleLogout}>
+                                <ListItemIcon><PowerSettingsNewIcon fontSize="small" /></ListItemIcon>
+                                <ListItemText primary="Logout" />
+                            </ListItem>
+                            <ListItem button onClick={accountMenuState.close} disabled={true}>
+                                <ListItemIcon><PersonIcon fontSize="small" /></ListItemIcon>
+                                <ListItemText primary="My Account" />
+                            </ListItem>
+                        </List>
+                    </Popover>
+                </div>
+            </Toolbar>
+        </AppBar>
+    );
 }
