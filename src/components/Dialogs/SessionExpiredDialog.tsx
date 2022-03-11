d3576be928c398af826b7b0c1645a660863eca65
Ed Shortt <edjash@gmail.com>
2022-03-11 15:39:00 +0000

    Session is nolong erroneously cleared

--- src/components/Dialogs/SessionExpiredDialog.tsx
+++ src/components/Dialogs/SessionExpiredDialog.tsx
@@ -14,8 +14,6 @@ export default function SesssionExpiredDialog() {
     const appContext = useAppContext();
 
     useEffect(() => {
-        clearSession();
-
         const sessionTimer = setInterval(() => {
             if (!csrfCookieExists()) {
                 setSessionExpired(true);
