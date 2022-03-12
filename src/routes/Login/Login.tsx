c474043d5c0f35eea49cfb7825bb3769a908b0cd
Ed Shortt <edjash@gmail.com>
2022-03-12 07:51:20 +0000

    Removed console log

--- src/routes/Login/Login.tsx
+++ src/routes/Login/Login.tsx
@@ -54,7 +54,6 @@ export default function Login() {
                 history.push('/');
             })
             .catch((response) => {
-                console.log("RRR");
                 setState({ ...state, isLoading: false });
                 lastError.current = `${response?.status}: ${response?.statusText} `
                     + response?.request?.responseText;
