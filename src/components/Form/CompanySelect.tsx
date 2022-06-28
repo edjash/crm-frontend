9ea1dc1ab816321f2a7eafac26497b34f2dddd1d
Ed Shortt <ed.shortt@gmail.com>
2022-06-28 15:56:53 +0100

    Fix dialog backdrop not displaying for child-dialog

--- src/components/Form/CompanySelect.tsx
+++ src/components/Form/CompanySelect.tsx
@@ -19,7 +19,6 @@ export function CompanySelect(props: CompanySelectProps) {
                     }
                 },
                 noAnimation: true,
-                hideBackdrop: true,
             })
         });
     }
